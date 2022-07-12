e2b801676bb1caed5f3a7970d0a04c3722118841
pn <pet.v.ne@gmail.com>
2022-07-12 21:05:39 +0200

    onchain: Add tests for GBitcoinEstimator

---
+++ onchain/estimator_test.go
@@ -0,0 +1,85 @@
+package onchain
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/elementsproject/glightning/gbitcoin"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGBitcoin_Estimator(t *testing.T) {
+	gbitcoinBackend := &GBitcoinBackendMock{}
+
+	// Set the PingReturn to true so that we mimic a connection.
+	gbitcoinBackend.PingReturn = true
+
+	feeEstimator, err := NewGBitcoindEstimator(
+		gbitcoinBackend,
+		"CONSERVATIVE",
+		FeePerKwFloor,
+	)
+	require.NoError(t, err)
+
+	// Set GetMempoolInfoReturn value for startup. This value is in BTC/kb
+	const mempoolMinFeeBTCPerKb = 0.000001
+	gbitcoinBackend.GetMempoolInfoReturn = &gbitcoin.MempoolInfo{
+		MempoolMinFee: mempoolMinFeeBTCPerKb,
+	}
+
+	// Start fee estimator.
+	require.NoError(t, feeEstimator.Start())
+
+	// Check that min fee manager is set. mempoolMinFee should be in sat/kw
+	mempoolMinFeeSatPerKb, _ := btcutil.NewAmount(mempoolMinFeeBTCPerKb)
+	mempoolMinFeeSatPerKw := mempoolMinFeeSatPerKb / 4
+	require.Equal(t, mempoolMinFeeSatPerKw, feeEstimator.minFeeManager.minFeePerKW)
+
+	// Check that fallback fee is returned if the feeEstimator returns an error
+	efe := fmt.Errorf("some error")
+	gbitcoinBackend.EstimateFeeError = &efe
+	fee, err := feeEstimator.EstimateFeePerKW(10)
+	require.NoError(t, err)
+	require.Equal(t, FeePerKwFloor, fee)
+}
+
+// GBitcoinBackendMock is a mock for the GBitcoinBackend interface that is the
+// RPC proxy we use to connect to bitcoind.
+type GBitcoinBackendMock struct {
+	GetMempoolInfoCalled int
+	GetMempoolInfoReturn *gbitcoin.MempoolInfo
+	GetMempoolInfoError  *error
+
+	EstimateFeeCalled int
+	EstimateFeeReturn *gbitcoin.FeeResponse
+	EstimateFeeError  *error
+
+	PingCalled int
+	PingReturn bool
+	PingError  *error
+}
+
+func (m *GBitcoinBackendMock) GetMempoolInfo() (*gbitcoin.MempoolInfo, error) {
+	m.GetMempoolInfoCalled++
+	if m.GetMempoolInfoError != nil {
+		return m.GetMempoolInfoReturn, *m.GetMempoolInfoError
+	}
+	return m.GetMempoolInfoReturn, nil
+}
+
+func (m *GBitcoinBackendMock) EstimateFee(blocks uint32, mode string) (*gbitcoin.FeeResponse, error) {
+	m.EstimateFeeCalled++
+	if m.EstimateFeeError != nil {
+		return m.EstimateFeeReturn, *m.EstimateFeeError
+	}
+	return m.EstimateFeeReturn, nil
+}
+
+func (m *GBitcoinBackendMock) Ping() (bool, error) {
+	m.PingCalled++
+	if m.PingError != nil {
+		return m.PingReturn, *m.PingError
+	}
+	return m.PingReturn, nil
+}
