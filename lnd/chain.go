7232e629b7d8b881de5d691908257904270e79e6
Peter Neuroth <pet.v.ne@gmail.com>
2022-08-15 21:27:19 +0200

    lnd: Add tests for transaction watcher

---
+++ lnd/chain.go
@@ -0,0 +1,30 @@
+package lnd
+
+import (
+	"context"
+	"errors"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/lightningnetwork/lnd/lnrpc"
+)
+
+func GetBitcoinChain(ctx context.Context, li lnrpc.LightningClient) (*chaincfg.Params, error) {
+	gi, err := li.GetInfo(ctx, &lnrpc.GetInfoRequest{})
+	if err != nil {
+		return nil, err
+	}
+	switch gi.Chains[0].Network {
+	case "regtest":
+		return &chaincfg.RegressionNetParams, nil
+	case "testnet":
+		return &chaincfg.TestNet3Params, nil
+	case "signet":
+		return &chaincfg.SigNetParams, nil
+	case "bitcoin":
+		return &chaincfg.MainNetParams, nil
+	case "mainnet":
+		return &chaincfg.MainNetParams, nil
+	default:
+		return nil, errors.New("unknown bitcoin network")
+	}
+}
