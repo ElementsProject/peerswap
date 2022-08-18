f49cc7c281ca8c63a99bd08ed234488689d33907
Peter Neuroth <pet.v.ne@gmail.com>
2022-08-18 03:21:34 +0200

    lnd: Move the connect constants to the connect file.

--- lnd/connect.go
+++ lnd/connect.go
@@ -3,13 +3,68 @@ package lnd
 import (
 	"context"
 	"io/ioutil"
+	"time"
 
 	"github.com/elementsproject/peerswap/cmd/peerswaplnd"
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"github.com/lightningnetwork/lnd/macaroons"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"gopkg.in/macaroon.v2"
+)
+
+const (
+	// defaultGrpcBackoffTime is the linear backoff time between failing grpc
+	// calls (also server side stream) to the lnd node.
+	defaultGrpcBackoffTime   = 1 * time.Second
+	defaultGrpcBackoffJitter = 0.1
+
+	// defaultMaxGrpcRetries is the amount of retries we take if the grpc
+	// connection to the lnd node drops for some reason or if the resource is
+	// exhausted. With the defaultGrpcBackoffTime and a linear backoff stratefgy
+	// this leads to roughly 5h of retry time.
+	defaultMaxGrpcRetries = 5
+)
+
+var (
+	// defaultGrpcRetryCodes are the grpc status codes that are returned with an
+	// error, on which we retry our call (and server side stream) to the lnd
+	// node. The codes represent:
+	// - Unavailable:	The service is currently unavailable. This is most
+	//					likely a transient condition, which can be correctesd by
+	//					retrying with a backoff. Note that it is not always safe
+	//					to retry non-idempotent operations.
+	//
+	// - ResourceExhausted:	Some resource has been exhausted, perhaps a per-user
+	//						quota, or perhaps the entire file system is out of
+	//						space.
+	defaultGrpcRetryCodes []codes.Code = []codes.Code{
+		codes.Unavailable,
+		codes.ResourceExhausted,
+	}
+
+	// defaultGrpcRetryCodesWithMsg are grpc status codes that must have a
+	// matching message for us to retry. This is due to LND using a confusing
+	// rpc error code on startup.
+	// See: https://github.com/lightningnetwork/lnd/issues/6765
+	//
+	// This is also the reason that we need to use a fork of the
+	// go-grpc-middleware "retry" to provide this optional check.
+	defaultGrpcRetryCodesWithMsg []grpc_retry.CodeWithMsg = []grpc_retry.CodeWithMsg{
+		{
+			Code: codes.Unknown,
+			Msg:  "the RPC server is in the process of starting up, but not yet ready to accept calls",
+		},
+		{
+			Code: codes.Unknown,
+			Msg:  "server is in the process of starting up, but not yet ready to accept calls",
+		},
+		{
+			Code: codes.Unknown,
+			Msg:  "chain notifier RPC is still in the process of starting",
+		},
+	}
 )
 
 func GetClientConnection(ctx context.Context, cfg *peerswaplnd.LndConfig) (*grpc.ClientConn, error) {
