d2dc880dfc330c328df3a290f808232b28033f39
Peter Neuroth <pet.v.ne@gmail.com>
2022-12-19 08:34:57 +0100

    lighting: Add Scid type with helper functions
    We often need to convert between the cln and lnd scid style where
    cln divides the parts of the scid with an `x` and lnd divides the
    parts with an `:`.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- lightning/lightning.go
+++ lightning/lightning.go
@@ -1,8 +1,22 @@
 package lightning
 
+import "strings"
+
 // Invoice defines the neccessary parts for decodepayreq
 type Invoice struct {
 	PHash       string
 	Amount      uint64
 	Description string
+}
+
+type Scid string
+
+// ClnStyle returns the `short_channel_id` divided by 'x'
+func (s Scid) ClnStyle() string {
+	return strings.ReplaceAll(string(s), ":", "x")
+}
+
+// LndStyle returns the `short_channel_id` divided by ':'
+func (s Scid) LndStyle() string {
+	return strings.ReplaceAll(string(s), "x", ":")
 }
