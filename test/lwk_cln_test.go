2a3b3f4ef885adbd9da69aec27ca3e11b320eac1
bruwbird <bruwbird@mercari.com>
2024-06-19 12:36:41 +0900

    test: add tests for backend down
    add tests for backend down scenarios in LWK.
    Add tests to handle scenarios where the backend services
    (LWK and Electrs) are down during swaps.

--- test/lwk_cln_test.go
+++ test/lwk_cln_test.go
@@ -1480,3 +1480,163 @@ func Test_ClnCln_LWKLiquid_SwapOut(t *testing.T) {
 		csvClaimTest(t, params)
 	})
 }
+
+func Test_ClnCln_LWKLiquid_BackendDown(t *testing.T) {
+	IsIntegrationTest(t)
+	t.Parallel()
+	t.Run("lwkdown", func(t *testing.T) {
+		t.Parallel()
+		require := require.New(t)
+
+		bitcoind, liquidd, lightningds, scid, electrs, lwk := clnclnLWKLiquidSetup(t, uint64(math.Pow10(9)))
+		defer func() {
+			if t.Failed() {
+				filter := os.Getenv("PEERSWAP_TEST_FILTER")
+				pprintFail(
+					tailableProcess{
+						p:     bitcoind.DaemonProcess,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:     liquidd.DaemonProcess,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:      lightningds[0].DaemonProcess,
+						filter: filter,
+						lines:  defaultLines,
+					},
+					tailableProcess{
+						p:      lightningds[1].DaemonProcess,
+						filter: filter,
+						lines:  defaultLines,
+					},
+					tailableProcess{
+						p:     electrs.Process,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:     lwk.Process,
+						lines: defaultLines,
+					},
+				)
+			}
+		}()
+
+		var channelBalances []uint64
+		var walletBalances []uint64
+		for _, lightningd := range lightningds {
+			b, err := lightningd.GetBtcBalanceSat()
+			require.NoError(err)
+			walletBalances = append(walletBalances, b)
+
+			b, err = lightningd.GetChannelBalanceSat(scid)
+			require.NoError(err)
+			channelBalances = append(channelBalances, b)
+		}
+
+		params := &testParams{
+			swapAmt:          channelBalances[0] / 2,
+			scid:             scid,
+			origTakerWallet:  walletBalances[0],
+			origMakerWallet:  walletBalances[1],
+			origTakerBalance: channelBalances[0],
+			origMakerBalance: channelBalances[1],
+			takerNode:        lightningds[0],
+			makerNode:        lightningds[1],
+			takerPeerswap:    lightningds[0].DaemonProcess,
+			makerPeerswap:    lightningds[1].DaemonProcess,
+			chainRpc:         liquidd.RpcProxy,
+			chaind:           liquidd,
+			confirms:         LiquidConfirms,
+			csv:              LiquidCsv,
+			swapType:         swap.SWAPTYPE_OUT,
+		}
+		asset := "lbtc"
+
+		lwk.Process.Kill()
+
+		// Do swap.
+		var response map[string]interface{}
+		err := lightningds[1].Rpc.Request(&clightning.SwapOut{SatAmt: params.swapAmt, ShortChannelId: params.scid, Asset: asset}, &response)
+		require.Error(err)
+	})
+	t.Run("electrsdown", func(t *testing.T) {
+		t.Parallel()
+		require := require.New(t)
+
+		bitcoind, liquidd, lightningds, scid, electrs, lwk := clnclnLWKLiquidSetup(t, uint64(math.Pow10(9)))
+		defer func() {
+			if t.Failed() {
+				filter := os.Getenv("PEERSWAP_TEST_FILTER")
+				pprintFail(
+					tailableProcess{
+						p:     bitcoind.DaemonProcess,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:     liquidd.DaemonProcess,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:      lightningds[0].DaemonProcess,
+						filter: filter,
+						lines:  defaultLines,
+					},
+					tailableProcess{
+						p:      lightningds[1].DaemonProcess,
+						filter: filter,
+						lines:  defaultLines,
+					},
+					tailableProcess{
+						p:     electrs.Process,
+						lines: defaultLines,
+					},
+					tailableProcess{
+						p:     lwk.Process,
+						lines: defaultLines,
+					},
+				)
+			}
+		}()
+
+		var channelBalances []uint64
+		var walletBalances []uint64
+		for _, lightningd := range lightningds {
+			b, err := lightningd.GetBtcBalanceSat()
+			require.NoError(err)
+			walletBalances = append(walletBalances, b)
+
+			b, err = lightningd.GetChannelBalanceSat(scid)
+			require.NoError(err)
+			channelBalances = append(channelBalances, b)
+		}
+
+		params := &testParams{
+			swapAmt:          channelBalances[0] / 2,
+			scid:             scid,
+			origTakerWallet:  walletBalances[0],
+			origMakerWallet:  walletBalances[1],
+			origTakerBalance: channelBalances[0],
+			origMakerBalance: channelBalances[1],
+			takerNode:        lightningds[0],
+			makerNode:        lightningds[1],
+			takerPeerswap:    lightningds[0].DaemonProcess,
+			makerPeerswap:    lightningds[1].DaemonProcess,
+			chainRpc:         liquidd.RpcProxy,
+			chaind:           liquidd,
+			confirms:         LiquidConfirms,
+			csv:              LiquidCsv,
+			swapType:         swap.SWAPTYPE_OUT,
+		}
+		asset := "lbtc"
+
+		electrs.Process.Kill()
+
+		// Do swap.
+		var response map[string]interface{}
+		err := lightningds[1].Rpc.Request(&clightning.SwapOut{SatAmt: params.swapAmt, ShortChannelId: params.scid, Asset: asset}, &response)
+		require.Error(err)
+	})
+
+}
